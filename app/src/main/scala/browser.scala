1e5d2cd9eb648ac25ceebd5f87701ff66dd603e4
Nathan Hamblen <nathan@technically.us>
2010-09-06 23:59:44 -0400

    merged app and processor project

---
+++ app/src/main/scala/browser.scala
@@ -0,0 +1,65 @@
+package com.example
+
+import unfiltered.request._
+import unfiltered.response._
+import unfiltered.server.Http
+
+import java.io.File
+import java.lang.ProcessBuilder
+
+/** unfiltered plan */
+class Browser(server: Http) extends unfiltered.Plan {
+  def dir(f: File) =
+    f.isDirectory && !f.getName.startsWith(".")
+
+  def project(f: File) = dir(f) && new File(f, "project/build.properties").exists
+
+  class PathExtract(predicate: File => Boolean) {
+    def unapply(path: String) = Some(new File(path)).filter(predicate)
+  }
+  object LocalPath extends PathExtract(dir)
+  object ProjectPath extends PathExtract(project)
+  def filter = {
+    case GET(Path(ProjectPath(path),_)) =>
+      new ProcessBuilder("sbt", "pilot").directory(path).start()
+      Redirect(path.getParent)
+    case GET(Path(LocalPath(path),_)) => Html(
+      <html>
+      <head><style>{ "li.project a { color: orange }" }</style></head>
+      <body>
+        <ul> {
+          val (projs, dirs) = path.list.toList.sortWith {
+            _.toUpperCase > _.toUpperCase
+          }.map { n =>
+            new File(path, n)
+          }.filter(dir).partition(project)
+          val all = projs.map { p => (p, "project") } ++
+                    dirs.map { d => (d, "dir") }
+          for ((d, cls) <- all) yield 
+            <li class={ cls }> <a href={ d.getAbsolutePath }>{ d.getName }</a> </li>
+          }
+        </ul>
+      </body></html>
+    )
+  }
+}
+
+/** embedded server */
+object Server {
+  def main(args: Array[String]) {
+    val port = 8080
+    val server = Http(port)
+    server.filter(new Browser(server)).run { server =>
+      val loc = "http://127.0.0.1:%d" format server.port
+      println("Pilot started at " + loc)
+      val home = System.getProperty("user.home")
+      try {
+        import java.net.URI
+        val dsk = Class.forName("java.awt.Desktop")
+        dsk.getMethod("browse", classOf[URI]).invoke(
+          dsk.getMethod("getDesktop").invoke(null), new URI(loc + home)
+        )
+      } catch { case e => () }
+    }
+  }
+}
--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,14 +1,16 @@
 import sbt._
 
-class Project(info: ProjectInfo) extends ProcessorProject(info) {
+class Project(info: ProjectInfo) extends ParentProject(info) {
   val uf_version = "0.1.5-SNAPSHOT"
-  
-  // unfiltered
-  lazy val uf = "net.databinder" %% "unfiltered-server" % uf_version
-  
-  lazy val df = "net.databinder" %% "dispatch-futures" % "0.7.5"
 
-  // testing
-  lazy val uf_spec = "net.databinder" %% "unfiltered-spec" % uf_version % "test"
+  lazy val processor = project("processor", "Pilot Processor", new ProcessorProject(_) {
+    override def buildScalaVersion = "2.7.7"
+    // unfiltered
+    lazy val uf = "net.databinder" %% "unfiltered-server" % uf_version
+    lazy val df = "net.databinder" %% "dispatch-futures" % "0.7.5"
+  })
+  lazy val app = project("app", "Pilot Application", new DefaultProject(_) {
+    lazy val uf = "net.databinder" %% "unfiltered-server" % uf_version
+  })
 }
 
