96e38280a60561c70d7048522fb2566ea79ad91a
Nathan Hamblen <nathan@technically.us>
2010-09-06 22:01:32 -0400

    find open port

--- src/main/scala/pilot.scala
+++ src/main/scala/pilot.scala
@@ -5,25 +5,37 @@ import unfiltered.response._
 import unfiltered.server.Http
 
 class Processor extends sbt.processor.BasicProcessor {
-  val port = 8080
   def apply(p: sbt.Project, s: String) = {
     (p) match {
       case (p: sbt.BasicScalaProject) =>
-        val s = Http(port);
-        s.filter(new Pilot(p,s)).run { server =>
-          val loc = "http://127.0.0.1:%d/" format server.port
-          try {
-            import java.net.URI
-            val dsk = Class.forName("java.awt.Desktop")
-            dsk.getMethod("browse", classOf[URI]).invoke(
-              dsk.getMethod("getDesktop").invoke(null), new URI(loc)
-            )
-          } catch { case e => () }
-          p.log.info("Started Pilot at " + loc)
+        Processor.findOpenPort(8080) { port =>
+          val s = Http(port)
+          s.filter(new Pilot(p,s)).run { server =>
+            val loc = "http://127.0.0.1:%d/" format server.port
+            try {
+              import java.net.URI
+              val dsk = Class.forName("java.awt.Desktop")
+              dsk.getMethod("browse", classOf[URI]).invoke(
+                dsk.getMethod("getDesktop").invoke(null), new URI(loc)
+              )
+            } catch { case e => () }
+            p.log.info("Started Pilot at " + loc)
+          }
         }
       case _ =>
         p.log.error("Can only pilot a BasicScalaProject")
     }
+  }
+}
+
+object Processor {
+  def findOpenPort[T](port: Int)(use: Int => T) = {
+    def attempt(p: Int): T = try {
+      use(p)
+    } catch {
+      case e: java.net.BindException if (p - port < 100) => attempt(p + 1)
+    }
+    attempt(port)
   }
 }
 
