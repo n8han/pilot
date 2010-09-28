410168766344e76ff0aa3beed2e1b37b979e10c6
Nathan Hamblen <nathan@technically.us>
2010-09-28 00:43:38 -0400

    remember processes we've forked

--- browser/src/main/scala/process.scala
+++ browser/src/main/scala/process.scala
@@ -1,12 +1,15 @@
 package pilot.browser
-import java.lang.ProcessBuilder
+import java.lang.{ProcessBuilder,Process => JProcess}
 import java.io.{File,BufferedReader,InputStreamReader}
 
 object Process {
   val Serving = """.*Serving: (http://\S+).*""".r
+  private var processes: List[JProcess] = Nil
   def pilot(path: File) = {
     val process = new ProcessBuilder("sbt", "pilot").directory(path).start()
-    /* @tailrec */ def handle(reader: BufferedReader): Option[String] =
+    processes = process :: processes
+    /* @tailrec */ 
+    def handle(reader: BufferedReader): Option[String] =
       reader.readLine() match {
         case null => None
         case Serving(loc) => Some(loc)
