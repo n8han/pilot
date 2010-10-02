c15519a28431bbaa07fbafc8aae0ae2876c28e39
Nathan Hamblen <nathan@technically.us>
2010-10-02 00:29:16 -0400

    async close button

--- processor/src/main/scala/processor.scala
+++ processor/src/main/scala/processor.scala
@@ -6,21 +6,31 @@ import unfiltered.jetty.Http
 
 class Processor extends sbt.processor.BasicProcessor {
   def apply(p: sbt.Project, s: String) = {
-    (p) match {
-      case (p: sbt.BasicScalaProject) =>
-        val s = Http(unfiltered.Port.any).resources(pilot.Shared.resources)
-        s.filter(new Pilot(p,s)).run { server =>
-          println("Serving: http://127.0.0.1:%d/" format server.port )
-        }
-      case _ =>
-        p.log.error("Can only pilot a BasicScalaProject")
+    val s = Http(unfiltered.Port.any).resources(pilot.Shared.resources)
+    s.filter(new Pilot(p,s)).run { server =>
+      println("Serving: http://127.0.0.1:%d/" format server.port )
     }
   }
 }
 
-class Pilot(project: sbt.BasicScalaProject, server: Http) extends
-    unfiltered.filter.Plan {
+class Pilot(project: sbt.Project, server: Http) extends unfiltered.filter.Plan {
   import dispatch.futures.DefaultFuture._
+  def intent = {
+    case GET(Path("/",_)) => pilot.Shared.page(
+      <h1>{ project.name }</h1>
+      <form method="POST">
+        <input type="submit" name="action" value="Exit" />
+      </form>
+
+    )
+    case POST(Path("/", Params(Action("Exit",_),_))) => 
+      future { Thread.sleep(500); server.stop() }
+      ResponseString("Exited")
+  }
+}
+object Action extends Params.Extract("action", Params.first ~> Params.nonempty)
+
+class Controls(project: sbt.BasicScalaProject, server: Http) {
   abstract class Button(val name: String) extends (() => Unit) {
     val html = <input type="submit" name="action" value={name} />
   }
@@ -33,20 +43,13 @@ class Pilot(project: sbt.BasicScalaProject, server: Http) extends
   object Clean extends Button("Clean") {
     def apply() { project.clean.run }
   }
-  object Exit extends Button("Exit") {
-    def apply() { future { Thread.sleep(500); server.stop() } }
-  }
   val buttons = (Map.empty[String, Button] /: (
-    Compile :: Run :: Clean :: Exit :: Nil
+    Compile :: Run :: Clean :: Nil
   )) { (m, a) => m + (a.name -> a) }
-  object Action extends Params.Extract("action", Params.first ~> Params.nonempty)
-  val action_panel = pilot.Shared.page(
-    <h1> { project.name } </h1>
-    <form method="POST">
-      { buttons.values.map { _.html } }
-    </form>
-  )
-  def intent = {
+  val action_panel =
+    <span>{ buttons.values.map { _.html } }</span>
+
+  def something[T]: PartialFunction[HttpRequest[T], scala.xml.NodeSeq] = {
     case GET(Path("/",_)) => action_panel
     case POST(Path("/", Params(Action(name,_),_))) => 
       buttons.get(name).foreach { _() }
--- shared/src/main/resources/web/js/browser.js
+++ shared/src/main/resources/web/js/browser.js
@@ -1,15 +1,24 @@
 $(function() {
-  $("li.project a").click(function() {
-    // open now so we're not blocked
-    var fork = window.open("", "_new");
-    $.getJSON(this.href + "?callback=?", function(res) {
-      if (res != "fail")
-         fork.location = res;
-      else {
-        fork.close();
-        alert("Pilot failed to take off.");
-      }
+    $("li.project a").click(function() {
+        // open now so we're not blocked
+        var fork = window.open("", "_new");
+        $.getJSON(this.href + "?callback=?", function(res) {
+            if (res != "fail")
+                fork.location = res;
+            else {
+                fork.close();
+                alert("Pilot failed to take off.");
+            }
+        });
+        return false;
     });
-    return false;
-  });
+    $("input[value=Exit]").click(function() {
+        $.ajax({
+            url: "/",
+            type: "POST",
+            data: { action: "Exit" }
+        });
+        window.close();
+        return false;
+    });
 });
