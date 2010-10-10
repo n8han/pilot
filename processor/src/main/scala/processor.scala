c684216702fac5add15ba347212aa52a9715d18b
Nathan Hamblen <nathan@technically.us>
2010-10-10 13:01:40 -0400

    move child rendering into methods

--- processor/src/main/scala/processor.scala
+++ processor/src/main/scala/processor.scala
@@ -4,6 +4,8 @@ import unfiltered.request._
 import unfiltered.response._
 import unfiltered.jetty.Http
 
+import java.io.File
+
 class Processor extends sbt.processor.BasicProcessor {
   def apply(p: sbt.Project, s: String) = {
     val s = Http(unfiltered.Port.any, "127.0.0.1").resources(pilot.Shared.resources)
@@ -18,46 +20,59 @@ class Pilot(project: sbt.Project, server: Http) extends unfiltered.filter.Plan {
 
   import dispatch.futures.DefaultFuture._
   def intent = {
-    case GET(Path(LocalPath(path),_)) => pilot.Shared.page(
-      <div class="prepend-top span-15 append-5 last">
-        <h1>{ project.name }</h1>
-        <form method="POST">
-          <input type="submit" name="action" value="Exit" />
-        </form>
-        <ul class="directory">{
-          import java.io.File
-          val (dirs,files) = Directory.children(path).filter { f =>
-            f.getName match {
-              case "lib_managed" | "target" => false
-              case name if name.startsWith(".") => false
-              case _ => true
-            }
-          }.partition(Directory.dir)
-          val project_path = project.info.projectPath.asFile.getCanonicalPath
-          val prefix = project_path.length
-          def relative(d: File) = d.getCanonicalPath match {
-            case `project_path` => "/"
-            case full => full.substring(prefix)
-          }
-          val parent_li = Some(path).filter { 
-            _.getCanonicalPath != project_path
-          }.map { d => new File(d.getParent) }.map { d =>
-            <li class="parent"> <a href={ relative(d) }>{ d.getName }</a> </li>
-          }
-          val dirs_li = dirs map { d =>
-            <li class="dir"> <a href={ relative(d) }>{ d.getName }</a> </li>
-          }
-          val files_li = files map { f =>
-            <li class="file">{ f.getName }</li>
-          }
-          parent_li ++ dirs_li ++ files_li
-        }</ul>
-
-      </div>
-    )
+    case GET(Path(LocalPath(path),_)) => 
+      val fly_project = flyable(path)
+      pilot.Shared.page(
+        <div class="prepend-top span-15 append-5 last">
+          <h1>{ project.name }</h1>
+          <form method="POST">
+            <input type="submit" name="action" value="Exit" />
+          </form>
+          <ul class="directory">{ children_li(path) }</ul>
+        </div>
+      )
     case POST(Path("/", Params(Action("Exit",_),_))) => 
       future { Thread.sleep(500); server.stop() }
       ResponseString("Exited")
+  }
+  def flyable(path: File): Option[sbt.BasicScalaProject] =
+    project match {
+      case project: sbt.BasicScalaProject => Some(project)
+      case project => 
+        val cur = path.getAbsolutePath
+        project.subProjects.values.find { p =>
+          cur.startsWith(p.info.projectPath.absolutePath)
+        }.flatMap {
+          case p: sbt.BasicScalaProject => Some(p)
+          case _ => None
+        }
+    }
+  def children_li(path: File) = {
+    val (dirs,files) = Directory.children(path).filter { f =>
+      f.getName match {
+        case "lib_managed" | "target" => false
+        case name if name.startsWith(".") => false
+        case _ => true
+      }
+    }.partition(Directory.dir)
+    val project_path = project.info.projectPath.absolutePath
+    val prefix = project_path.length
+    def relative(d: File) = d.getAbsolutePath match {
+      case `project_path` => "/"
+      case full => full.substring(prefix)
+    }
+    val parent_li = Some(path).filter { 
+      _.getAbsolutePath != project_path
+    }.map { d => new File(d.getParent) }.map { d =>
+      <li class="parent"> <a href={ relative(d) }>{ d.getName }</a> </li>
+    }
+    val dirs_li = dirs map { d =>
+      <li class="dir"> <a href={ relative(d) }>{ d.getName }</a> </li>
+    }
+    val files_li = files map { f =>
+      <li class="file">{ f.getName }</li>
+    }
+    parent_li ++ dirs_li ++ files_li
   }
 }
 object Action extends Params.Extract("action", Params.first ~> Params.nonempty)
