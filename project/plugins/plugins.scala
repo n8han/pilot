30da4855c27b8aa13cea999d7d1d89b077930bd8
Nathan Hamblen <nathanh@meetup.com>
2010-11-01 12:16:49 -0400

    need to reference non-repo trans dependency

--- project/plugins/plugins.scala
+++ project/plugins/plugins.scala
@@ -3,4 +3,6 @@ import sbt._
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val coffeeScriptSbtRepo = "coffeeScript sbt repo" at "http://repo.coderlukes.com/"
   val coffeeScript = "org.coffeescript" % "coffee-script-sbt-plugin" % "0.9.0"
+  val jcoffeescript = "org.jcoffeescript" % "jcoffeescript" % "0.9.0" from
+    "http://github.com/downloads/yeungda/jcoffeescript/jcoffeescript-0.9.0.jar"
 }
