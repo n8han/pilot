2cb640fb67178bbfa432c3d74070e1b6bb274142
Nathan Hamblen <nathan@technically.us>
2010-11-13 15:35:57 -0500

    ivy won't follow github's redirect

--- project/plugins/plugins.scala
+++ project/plugins/plugins.scala
@@ -4,5 +4,5 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val coffeeScriptSbtRepo = "coffeeScript sbt repo" at "http://repo.coderlukes.com/"
   val coffeeScript = "org.coffeescript" % "coffee-script-sbt-plugin" % "0.9.0"
   val jcoffeescript = "org.jcoffeescript" % "jcoffeescript" % "0.9.0" from
-    "http://github.com/downloads/yeungda/jcoffeescript/jcoffeescript-0.9.0.jar"
+    "http://cloud.github.com/downloads/yeungda/jcoffeescript/jcoffeescript-0.9.0.jar"
 }
