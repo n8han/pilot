8c41f8fbeb3cf9351c2faf42d19efd9d4fa186a4
Nathan Hamblen <nathan@technically.us>
2010-11-26 21:39:14 -0500

    g8 lib version

--- project/plugins/plugins.scala
+++ project/plugins/plugins.scala
@@ -5,5 +5,5 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val coffeeScript = "org.coffeescript" % "coffee-script-sbt-plugin" % "0.9.0"
   val jcoffeescript = "org.jcoffeescript" % "jcoffeescript" % "0.9.0" from
     "http://cloud.github.com/downloads/yeungda/jcoffeescript/jcoffeescript-0.9.0.jar"
-  val g8lib = "net.databinder" %% "giter8-library" % "0.2.0-SNAPSHOT"
+  val g8lib = "net.databinder" %% "giter8-library" % "0.2.0"
 }
